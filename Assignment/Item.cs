2878d402b19db4731fdd4e2ebac423b3b48a1557
pearcd08 <pearcd08@myunitec.ac.nz>
2021-11-01 14:23:39 +1300

    Add project files.

---
+++ Assignment/Item.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Assignment
+{
+    class Item
+    {
+        public String itemName;
+        public decimal itemPrice;
+        private Random rand = new Random();
+        GameManager chest = new GameManager();
+
+        public Item(String tempName)
+        {
+            this.itemName = tempName;
+            this.itemPrice = getRandomPrice();
+           
+        }
+
+        public Item(String tempName, int tempPrice)
+        {
+            this.itemName = tempName;            
+            this.itemPrice = tempPrice;
+        }
+
+
+
+        public decimal getRandomPrice()
+        {
+            decimal price = rand.Next(1,99);
+            return price;
+        }
+
+
+
+
+    }
+}
---
+++ Assignment/SFML.cs
@@ -0,0 +1,98 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace Assignment
+{
+    class SFML
+    {
+        private RenderWindow window;
+        private VideoMode mode = new VideoMode(840, 480);
+        Hero hero;
+        GameManager game;
+
+        public SFML()
+        {
+            this.window = new RenderWindow(this.mode, "Game Inventory System");
+            this.window.SetVerticalSyncEnabled(true);
+            this.window.Closed += (sender, args) =>
+            {
+                this.window.Close();
+            };
+
+            hero = new Hero();
+            game = new GameManager();
+        }
+
+        public void Run()
+        {
+            // add the items into the game memory
+            game.addItem();
+
+            // mouse events
+            this.window.MouseButtonPressed += (sender, e) =>
+            {
+                Vector2f mousePos = this.window.MapPixelToCoords(new Vector2i(e.X, e.Y));
+                if (game.sortButton.GetGlobalBounds().Contains(mousePos.X, mousePos.Y))
+                {
+                    game.bubbleSortByName(game.inventoryList);
+                }
+                else if (game.sortButton2.GetGlobalBounds().Contains(mousePos.X, mousePos.Y))
+                {
+                    game.bubbleSortByPrice(game.inventoryList);
+                }
+            };
+
+            //inventory events
+            bool keyI = Keyboard.IsKeyPressed(Keyboard.Key.I);
+            bool keyO = Keyboard.IsKeyPressed(Keyboard.Key.O);
+
+            bool keyPressed = keyI || keyO;
+
+            if (keyPressed)
+            {
+                if (keyI)
+                {
+                    game.bubbleSortByName(game.inventoryList);
+                }
+                else if (keyO)
+                {
+
+                    game.bubbleSortByPrice(game.inventoryList);
+                }
+            }
+
+            
+            while (this.window.IsOpen)
+            {
+                this.Update();
+                this.Draw();
+                this.HandleEvents();
+            }
+        }
+
+        public void HandleEvents()
+        {
+            this.window.DispatchEvents();
+        }
+
+        public void Update()
+        {
+            this.game.addChest();
+            this.game.update(hero);
+            this.hero.update();
+        }
+
+        public void Draw()
+        {
+            this.window.Clear(Color.Black);
+            this.game.draw(this.window);
+            this.hero.draw(this.window);
+            this.window.Display();
+        }
+    }
+}
+
+
+
+
